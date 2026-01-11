0e8adf63818fe1257e9d0a36b37226f8632b7ece
AndyyBanh <andybanhab@gmail.com>
2026-01-11 17:24:49 -0500

    Added recent project

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -12,6 +12,44 @@ const Project = () => {
         <div className='grid grid-cols-2 justify-center gap-4 w-full max-w-2xl mx-auto'>
           <Card className='p-5 hover:border-blue-300'>
             <CardTitle>
+              Form Coach
+            </CardTitle>
+            <CardDescription>
+              Full Stack computer vision platform that allows users to correct their workout forms in real time using WebSockets to stream frames and computer vision to perform movement analysis.
+            </CardDescription>
+
+            <div className='flex flex-wrap gap-3'>
+              <Badge variant='secondary' className='shadow-2xl'>
+                Spring Boot
+              </Badge>
+              <Badge variant='secondary' className='shadow-2xl'>
+                Next.js
+              </Badge>
+              <Badge variant='secondary' className='shadow-2xl'>
+                Fast API
+              </Badge>
+              <Badge variant='secondary' className='shadow-2xl'>
+                OpenCV
+              </Badge>
+              <Badge variant='secondary' className='shadow-2xl'>
+                MediaPipe
+              </Badge>
+              <Badge variant='secondary' className='shadow-2xl'>
+                Supabase
+              </Badge>
+            </div>
+            <div>
+              <a
+                href='https://github.com/AndyyBanh/formcoach'
+                aria-label='Bathroom Buddy Project'
+                className='flex items-center gap-1.5 hover:text-blue-300'
+              >
+                <FaGithub /> Github
+              </a>
+            </div>
+          </Card>
+          <Card className='p-5 hover:border-blue-300'>
+            <CardTitle>
               Bathroom Buddy
             </CardTitle>
             <CardDescription>
@@ -20,7 +58,7 @@ const Project = () => {
 
             <div className='flex flex-wrap gap-3'>
               <Badge variant='secondary' className='shadow-2xl'>
-                SpringBoot
+                Spring Boot
               </Badge>
               <Badge variant='secondary' className='shadow-2xl'>
                 React
@@ -32,7 +70,6 @@ const Project = () => {
                 Supabase
               </Badge>
             </div>
-
             <div>
               <a
                 href='https://github.com/AndyyBanh/bathroombuddy'
@@ -42,7 +79,6 @@ const Project = () => {
                 <FaGithub /> Github
               </a>
             </div>
-
           </Card>
             
           <Card className='p-5 hover:border-blue-300'>
