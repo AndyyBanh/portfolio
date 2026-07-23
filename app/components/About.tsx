238153d259b9ec8f55f59defda8bd1498538c8fd
AndyyBanh <andybanhab@gmail.com>
2026-07-23 13:24:56 -0400

    bug fix

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image';
 import React from 'react'
 import { FaDownload } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
@@ -8,7 +7,7 @@ import { MdEmail } from "react-icons/md";
 const About = () => {
   return (
     <div className='grid grid-cols-1'>
-        <h2 className='text-2xl font-bold text-center'>hi i'm andy</h2>
+        <h2 className='text-2xl font-bold text-center'>hi i&apos;m andy</h2>
         <p className='text-sm text-center'>CS @YorkU</p>
         <p className='text-xs text-center'>Toronto, Canada</p>
         <p className='mt-2 text-center text-base max-w-md mx-auto'>typical cs student who spends countless hours on leetcode and building</p>
